750d4c9ab6ca9c691356e095feea660bd6266a4c
iyadshobaki <iyadshobaki82@gmail.com>
2025-12-29 18:16:59 -0500

    feat: add route to Dashboard componenet as default route /

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
