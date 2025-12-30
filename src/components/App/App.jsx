34e4f63c5bcfa620d9ecfb298244de3d080dd5ce
iyadshobaki <iyadshobaki82@gmail.com>
2025-12-29 19:18:36 -0500

    feat: add about me nested components and add nested routes to them

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,9 @@ import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
 import AboutUs from "../AboutUs/AboutUs";
 import "./App.css";
 function App() {
@@ -12,7 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
         <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </div>
